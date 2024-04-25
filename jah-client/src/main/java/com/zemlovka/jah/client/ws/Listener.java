717a08eab7e2388fd2b842cfdc52328eaf6ffd08
Mykhailo Bubnov <applemix00@gmail.com>
2024-04-25 23:29:46 +0200

    Updated ws files on client

--- jah-client/src/main/java/com/zemlovka/jah/client/ws/Listener.java
+++ jah-client/src/main/java/com/zemlovka/jah/client/ws/Listener.java
@@ -8,48 +8,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
 public class Listener implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(Listener.class);
-    private static final int MAX_CONNECTION_DOWN_S = 30;
-    private static final int MAX_RECONNECT_ATTEMPTS = 30;
-    private static final int RECONNECT_INTERVAL_S = 1;
-    private final InputStream InputStream;
-    private final Main mainProcess;
+    private InputStream inputStream;
 
-    public Listener(InputStream InputStream, Main mainProcess) {
-        this.InputStream = InputStream;
-        this.mainProcess = mainProcess;
+    public Listener(InputStream inputStream) {
+        this.inputStream = inputStream;
     }
 
     @Override
     public void run() {
         try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(InputStream))) {
-            int reconnectAttempt = 0;
-            Long connectionDownTime = null;
+                new InputStreamReader(inputStream))) {
             while (true) {
-                String readLine = br.readLine();
-                if (readLine != null) {
-                    System.out.println(readLine);
-                } else {
-                    if (connectionDownTime == null)
-                        connectionDownTime = System.currentTimeMillis();
-                    if (reconnectAttempt == MAX_RECONNECT_ATTEMPTS ||
-                            System.currentTimeMillis() - connectionDownTime > MAX_CONNECTION_DOWN_S * 1000) {
-                        log.info("Connection lost, disconnecting...");
-                        break;
-                    }
-                    log.info("Recconnect attempt {}", ++reconnectAttempt);
-                    Thread.sleep(RECONNECT_INTERVAL_S * 1000);
-                }
+                System.out.println(br.readLine());
             }
         } catch (IOException e) {
             log.error("Exception occurred while listening for incoming communication.", e);
-        } catch (InterruptedException e) {
-            log.error("Exception occurred while putting thread to sleep", e);
-        } finally {
-            mainProcess.end();
         }
     }
 }
