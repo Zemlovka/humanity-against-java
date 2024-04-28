4eaecbfe350f9c99486bebd49b2dbb7cb0d6c5c3
Nikita Korotov <korn03@vse.cz>
2024-04-28 06:20:00 +0200

    Login menu close-to-example css

--- haj-client/src/main/java/com/zemlovka/haj/client/fx/HelloApplication.java
+++ haj-client/src/main/java/com/zemlovka/haj/client/fx/HelloApplication.java
@@ -3,6 +3,7 @@ package com.zemlovka.haj.client.fx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/zemlovka/haj/client/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
+        scene.getStylesheets().add(getClass().getResource("/com/zemlovka/haj/client/styles.css").toExternalForm());
+        Font.loadFont(getClass().getResourceAsStream("/com/zemlovka/haj/client/Montserrat/static/Montserrat-Regular.ttf"), 14); // Load font collection
+        //Font regularFont = Font.loadFont(getClass().getResourceAsStream("/YourFontCollection.ttf#Regular"), 14);
+        //Font boldFont = Font.loadFont(getClass().getResourceAsStream("/YourFontCollection.ttf#Bold"), 14);
+        //Font italicFont = Font.loadFont(getClass().getResourceAsStream("/YourFontCollection.ttf#Italic"), 14);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
