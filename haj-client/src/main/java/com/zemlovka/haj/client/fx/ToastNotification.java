b327633aa9707dcd86ab52befa0b52e236c7f6da
Nikita Korotov <korn03@vse.cz>
2024-06-10 16:04:02 +0200

    Complete toasts

--- haj-client/src/main/java/com/zemlovka/haj/client/fx/ToastNotification.java
+++ haj-client/src/main/java/com/zemlovka/haj/client/fx/ToastNotification.java
@@ -6,73 +6,116 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Popup;
+import javafx.stage.Window;
 import javafx.util.Duration;
 
 public class ToastNotification {
+    private final Popup popup;
+    private final Window owner;
+    private final String message;
+    private final Position position;
+    private final boolean autoHide;
+    private final boolean canClose;
+    private final boolean isError; // New field to indicate if it's an error popup
+
     public enum Position {
         CENTER,
         RIGHT_BOTTOM
     }
-
-    public static void showToast(VBox owner, String message) {
-        showToast(owner, message, Position.CENTER, true, false);
+    public ToastNotification(Window owner, String message, Position position, boolean autoHide, boolean canClose) {
+        this.popup = new Popup();
+        this.owner = owner;
+        this.message = message;
+        this.position = position;
+        this.autoHide = autoHide;
+        this.canClose = canClose;
+        this.isError = false;
     }
 
-    public static void showToast(VBox owner, String message, Position position, boolean autoHide, boolean canClose) {
-        Platform.runLater(() -> {
-            Popup popup = new Popup();
-            double x = 0;
-            double y = 0;
+    public ToastNotification(Window owner, String message, Position position, boolean autoHide, boolean canClose, boolean isError) {
+        this.popup = new Popup();
+        this.owner = owner;
+        this.message = message;
+        this.position = position;
+        this.autoHide = autoHide;
+        this.canClose = canClose;
+        this.isError = isError;
+    }
 
+    public void showToast() {
+        Platform.runLater(() -> {
             Label label = new Label(message);
             label.setStyle("-fx-text-fill: white;");
             label.setFont(new Font("Arial", 16));
 
             HBox notificationBox = new HBox();
             notificationBox.getChildren().add(label);
-            notificationBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0.7); -fx-background-radius: 10; -fx-padding: 20px;");
+            if (isError) {
+                notificationBox.setStyle("-fx-background-color: rgba(119,17,17,0.5); -fx-background-radius: 10; -fx-padding: 20px;");
+            } else {
+                notificationBox.setStyle("-fx-background-color: rgba(0, 0, 0, 0.7); -fx-background-radius: 10; -fx-padding: 20px;");
+            }
             notificationBox.setAlignment(Pos.CENTER);
             notificationBox.setSpacing(10);
 
             if (autoHide) {
-                //popup.setAutoHide(true);
-                FadeTransition fadeTransition = new FadeTransition(Duration.seconds(4), notificationBox);
+                FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), notificationBox);
                 fadeTransition.setFromValue(1.0);
                 fadeTransition.setToValue(0.0);
                 fadeTransition.setOnFinished(event -> popup.hide());
                 fadeTransition.play();
             }
             if (canClose) {
-                notificationBox.getChildren().add(createCloseButton(popup));
+                notificationBox.getChildren().add(createCloseButton());
             }
-            if (position == Position.RIGHT_BOTTOM) {
-                y = owner.getScene().getWindow().getHeight() - 100;
-                x = owner.getScene().getWindow().getWidth() - 60;
-            }
-            if (position == Position.CENTER) {
-                y = owner.getScene().getWindow().getY() + 80;
-                x = owner.getScene().getWindow().getWidth() / 2;
-            }
+
             popup.getContent().add(notificationBox);
-            popup.setX(x);
-            popup.setY(y);
-            popup.show(owner.getScene().getWindow());
+            popup.show(owner);
+            updatePopupPosition();
+
+            // Add listener to update position when stage is resized or move
+            owner.xProperty().addListener((observable, oldValue, newValue) -> updatePopupPosition());
+            owner.yProperty().addListener((observable, oldValue, newValue) -> updatePopupPosition());
+            owner.widthProperty().addListener((observable, oldValue, newValue) -> updatePopupPosition());
+            owner.heightProperty().addListener((observable, oldValue, newValue) -> updatePopupPosition());
         });
     }
 
-    private static Button createCloseButton(Popup popup) {
+    public void closeToast() {
+        Platform.runLater(() -> {
+            if (popup.isShowing()) {
+                FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), popup.getContent().get(0));
+                fadeOut.setFromValue(1.0);
+                fadeOut.setToValue(0.0);
+                fadeOut.setOnFinished(e -> popup.hide());
+                fadeOut.play();
+            }
+        });
+    }
+
+    private Button createCloseButton() {
         Button closeButton = new Button("X");
         closeButton.setStyle("-fx-background-radius: 50%; -fx-font-size: 12;");
-        closeButton.setOnAction(event -> {
-            FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), popup.getContent().get(0));
-            fadeOut.setFromValue(1.0);
-            fadeOut.setToValue(0.0);
-            fadeOut.setOnFinished(e -> popup.hide());
-            fadeOut.play();
-        });
+        closeButton.setOnAction(event -> closeToast());
         return closeButton;
+    }
+
+    private void updatePopupPosition() {
+        double x = 0;
+        double y = 0;
+
+        if (position == Position.RIGHT_BOTTOM) {
+            x = owner.getX() + owner.getWidth() - this.popup.getWidth() - 40;
+            y = owner.getY() + owner.getHeight() - this.popup.getHeight() - 40;
+        }
+        if (position == Position.CENTER) {
+            x = owner.getX() + owner.getWidth() / 2 - this.popup.getWidth() / 2;
+            y = owner.getY() + 80;
+        }
+        popup.setX(x);
+        popup.setY(y);
     }
 }
